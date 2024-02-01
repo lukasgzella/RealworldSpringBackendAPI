b597bf3c894ee5b10665bda4cb5dc19a93148caf
lukas.gzella <lukas.gzella@gmail.com>
2024-02-01 21:48:24 +0100

    endpoints security update

--- src/main/java/com/hibernateRealworldRelations/realworldRelations/_config/SecurityConfiguration.java
+++ src/main/java/com/hibernateRealworldRelations/realworldRelations/_config/SecurityConfiguration.java
@@ -4,13 +4,16 @@ package com.hibernateRealworldRelations.realworldRelations._config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -20,15 +23,29 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/console/**").permitAll()
-                        .anyRequest().permitAll())
-                .headers(h -> h.frameOptions(Customizer.withDefaults()).disable())
-                .sessionManagement(Customizer.withDefaults())
+                        .requestMatchers(HttpMethod.POST,"/api/users/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/users").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/user").authenticated()
+                        .requestMatchers(HttpMethod.PUT,"/api/user").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/api/profiles/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/profiles/*/follow").authenticated()
+                        .requestMatchers(HttpMethod.DELETE,"/api/profiles/*/unfollow").authenticated()
+//                        .anyRequest().permitAll()
+                )
 
+                .headers(h -> h.frameOptions(Customizer.withDefaults()).disable())
+
+                .sessionManagement(Customizer.withDefaults())
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
