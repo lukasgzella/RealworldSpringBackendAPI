bbfb434e2065058997ac745c587c8430f163a710
lukas.gzella <lukas.gzella@gmail.com>
2024-03-18 23:06:16 +0100

    implement ProfileResponse

---
+++ src/main/java/com/hibernateRealworldRelations/realworldRelations/dto/responses/ProfileResponse.java
@@ -0,0 +1,22 @@
+package com.hibernateRealworldRelations.realworldRelations.dto.responses;
+
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonRootName("profile")
+public class ProfileResponse {
+
+    private String username;
+    private String bio;
+    private String image;
+    private boolean following;
+
+}
