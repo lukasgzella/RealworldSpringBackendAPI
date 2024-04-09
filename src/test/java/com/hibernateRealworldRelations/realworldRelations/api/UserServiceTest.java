7270ffdda0aba95f3406854e0d25bbbc720862a2
lukas.gzella <lukas.gzella@gmail.com>
2024-04-09 22:23:54 +0200

    implement registerUser_allParamsOk

--- src/test/java/com/hibernateRealworldRelations/realworldRelations/api/UserServiceTest.java
+++ src/test/java/com/hibernateRealworldRelations/realworldRelations/api/UserServiceTest.java
@@ -3,11 +3,14 @@ package com.hibernateRealworldRelations.realworldRelations.api;
 import com.hibernateRealworldRelations.realworldRelations.API.services.UserService;
 import com.hibernateRealworldRelations.realworldRelations._config.JwtService;
 import com.hibernateRealworldRelations.realworldRelations.auxiliary.AuthenticationFacade;
+import com.hibernateRealworldRelations.realworldRelations.dto.requests.RegistrationRequest;
+import com.hibernateRealworldRelations.realworldRelations.dto.responses.LoginResponse;
 import com.hibernateRealworldRelations.realworldRelations.entity.Role;
 import com.hibernateRealworldRelations.realworldRelations.entity.User;
 import com.hibernateRealworldRelations.realworldRelations.repository.FollowerRepository;
 import com.hibernateRealworldRelations.realworldRelations.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -51,23 +54,35 @@ public class UserServiceTest {
                 .build();
     }
 
+    @Test
     public void registerUser_allParamsOk_userRegistered() {
 
         //given
-        //RegistrationRequest
-        User actual = User.builder()
+        RegistrationRequest request = RegistrationRequest.builder()
                 .username("JohnDoe")
                 .email("johndoe@johndoe.pl")
                 .password("password")
+                .build();
+        var user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER)
                 .build();
-        User expected = initUser();
-        when(userRepository.existsByUsername(actual.getUsername())).thenReturn(false);
-        when(passwordEncoder.encode(actual.getPassword())).thenReturn("encodedPassword");
+        LoginResponse expected = LoginResponse.builder()
+                .email("johndoe@johndoe.pl")
+                .token("generatedToken")
+                .username("JohnDoe")
+                .bio(null)
+                .image(null)
+                .build();
+        when(userRepository.save(user)).thenReturn(user);
+        when(passwordEncoder.encode(request.getPassword())).thenReturn("encodedPassword");
+        when(jwtService.generateToken(user)).thenReturn("generatedToken");
         //when
-        userService.registerUser(actual);
+        LoginResponse actual = userService.registerUser(request);
         //then
         assertEquals(expected, actual);
-        verify(userRepository).save(expected);
+        verify(userRepository).save(user);
     }
 }
